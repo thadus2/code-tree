c8d31dc7717ea11740778cc33d98bedd37bc1302
thadus2 <152844617+thadus2@users.noreply.github.com>
2025-10-15 04:12:21 +0900

    Title: 변수 선언하기, Time: 87ms, Memory: 9MB, Status: Passed - Codetree

--- 251015/변수 선언하기/declaring-variables.java
+++ 251015/변수 선언하기/declaring-variables.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         // Please write your code here.
         int a = 3;
-        String b = 'C';
+        String b = "C";
         System.out.println(a);
         System.out.println(b);
     }
