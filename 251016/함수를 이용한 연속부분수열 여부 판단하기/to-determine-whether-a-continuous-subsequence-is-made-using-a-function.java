a2c186a315d1560a17c63262baed4979fcf83232
thadus2 <152844617+thadus2@users.noreply.github.com>
2025-10-16 23:36:01 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 127ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 251016/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Main {
+    private static String isPart(int a[], int b[], int n1, int n2) {
+        int cnt = 0;
+        for (int i = 0; i < n1; i++) {
+            for (int j = 0; j < n2; j++) {
+                if (a[i + j] != b[j]) {
+                    cnt = 0;
+                    break;
+                }
+                cnt += 1;
+            }
+            if (cnt == n2)
+                return "Yes";
+        }
+        return "No";
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int[] a = new int[n1];
+        int[] b = new int[n2];
+        for (int i = 0; i < n1; i++)
+            a[i] = sc.nextInt();
+        for (int i = 0; i < n2; i++)
+            b[i] = sc.nextInt();
+        // Please write your code here.
+        System.out.println(isPart(a, b, n1, n2));
+    }
+}
