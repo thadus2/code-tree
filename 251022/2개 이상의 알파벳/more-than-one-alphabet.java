6149df6c849bf2db2b7c523bb1deda5ea50d0e6c
thadus2 <152844617+thadus2@users.noreply.github.com>
2025-10-22 13:28:50 +0900

    Title: 2개 이상의 알파벳, Time: 122ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251022/2개 이상의 알파벳/more-than-one-alphabet.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    private static boolean func(String s) {
+        boolean flag = false;
+        for (int i = 0; i < s.length() - 1; i++) {
+            for (int j = 0; j < s.length() - 1; j++) {
+                if (s.charAt(i) != s.charAt(j)) {
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag)
+                return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        // Please write your code here.
+        if (func(A))
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
+}
