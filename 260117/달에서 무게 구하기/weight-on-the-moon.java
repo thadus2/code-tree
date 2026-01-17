bce804a5f3bc3196277a3df451209bf4a58cc38c
thadus2 <152844617+thadus2@users.noreply.github.com>
2026-01-17 22:46:52 +0900

    Title: 달에서 무게 구하기, Time: 94ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260117/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        System.out.print("13 * 0.165000 = ");
+        System.out.printf("%.6f", (13*0.165));
+    }
+}
