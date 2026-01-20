c14c16682a69e4c0df50885ba805f147b46d78ad
thadus2 <152844617+thadus2@users.noreply.github.com>
2026-01-20 23:28:32 +0900

    Title: 키, 몸무게를 기준으로 정렬, Time: 132ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260120/키, 몸무게를 기준으로 정렬/sort-by-height-and-weight.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class Person implements Comparable<Person> {
+    String name;
+    int height, weight;
+
+    public Person(String name, int height, int weight) {
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Person person) {
+        if (this.height == person.height)
+            return person.weight - this.weight;
+        return this.height - person.height;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Person[] persons = new Person[n];
+        for (int i = 0; i < n; i++) {
+            String name = sc.next();
+            int height = sc.nextInt();
+            int weight = sc.nextInt();
+            persons[i] = new Person(name, height, weight);
+            // Please write your code here.
+        }
+
+        Arrays.sort(persons);
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(persons[i].name + " " + persons[i].height + " " + persons[i].weight);
+        }
+    }
+}
