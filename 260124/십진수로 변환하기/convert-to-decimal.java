98f98b1beba2dcffe4aec98fe1066373cfbe310e
thadus2 <152844617+thadus2@users.noreply.github.com>
2026-01-24 23:17:56 +0900

    Title: 십진수로 변환하기, Time: 113ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260124/십진수로 변환하기/convert-to-decimal.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String binary = sc.next();
+        int result = 0;
+
+        for (int i = 0; i < binary.length(); i++) {
+            result = result * 2 + (binary.charAt(i) - '0');
+        }
+
+        System.out.println(result);
+    }
+}
