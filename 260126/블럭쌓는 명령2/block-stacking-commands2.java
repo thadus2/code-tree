1f9bbe0a23e6e4e868ab315ed01436bb82707abb
thadus2 <152844617+thadus2@users.noreply.github.com>
2026-01-26 23:43:09 +0900

    Title: 블럭쌓는 명령2, Time: 121ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 260126/블럭쌓는 명령2/block-stacking-commands2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int[] block = new int[N];
+        for (int i = 0; i < K; i++) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            for (int j = A - 1; j < B; j++) {
+                block[j]++;
+            }
+        }
+        // Please write your code here.
+        int max = 0;
+        for (int i = 0; i < K; i++) {
+            if (block[i] > max)
+                max = block[i];
+        }
+        System.out.println(max);
+    }
+}
