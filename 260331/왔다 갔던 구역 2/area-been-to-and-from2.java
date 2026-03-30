9dd2530b7fcfae1ef6ab55ff9c77f3b15da0d89c
thadus2 <152844617+thadus2@users.noreply.github.com>
2026-03-31 02:01:43 +0900

    Title: 왔다 갔던 구역 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260331/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 260331/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -1,30 +1,65 @@
 import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
         int N = sc.nextInt();
+
         int line[] = new int[2050];
+
         int curr = 1000;
+
         for (int i = 0; i < N; i++) {
+
             int x = sc.nextInt();
+
             char dir = sc.next().charAt(0);
+
             // Please write your code here.
+
             if (dir == 'L') {
-                for (int j = curr - x; j < curr; j++)
-                    line[j]++;
+
+                line[curr]--;
+
+                line[curr - x]++;
+
                 curr -= x;
+
             }
+
             if (dir == 'R') {
-                for (int j = curr; j < curr + x; j++)
-                    line[j]++;
+
+                line[curr]++;
+
+                line[curr + x]--;
+
                 curr += x;
-            }
+
+            }                
+
         }
+
         int sum = 0;
-        for (int i = 0; i < 2050; i++) {
-            if (line[i] > 1)
-                sum++;
+
+        for (int i = 1; i < 2050; i++) {
+
+            line[i] += line[i - 1];
+
         }
+
+        for (int i = 0; i < 2050; i++) {
+
+            if (line[i] > 1)
+
+                sum++;
+
+        }
+
         System.out.println(sum);
+
     }
+
 }
