b99f51ead2dfe9136ec8638dff4cb66a54e86871
thadus2 <152844617+thadus2@users.noreply.github.com>
2026-04-08 23:46:46 +0900

    Title: 색종이의 총 넓이, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260408/색종이의 총 넓이/the-total-area-of-colored-paper.java
+++ 260408/색종이의 총 넓이/the-total-area-of-colored-paper.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
 public class Main {
+    private static final int OFFSET = 100;
+    private static final int MAX_SIZE = 2 * OFFSET + 5;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[][] board = new int[201][201];
+        int[][] board = new int[MAX_SIZE][MAX_SIZE];
         for (int i = 0; i < n; i++) {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
+            int x = sc.nextInt() + OFFSET;
+            int y = sc.nextInt() + OFFSET;
 
             for (int dx = x; dx < x + 8; dx++) {
                 for (int dy = y; dy < y + 8; dy++)
@@ -15,8 +17,8 @@ public class Main {
         }
         // Please write your code here.
         int volume = 0;
-        for (int i = 0; i < 201; i++) 
-            for (int j = 0; j < 201; j++)
+        for (int i = 0; i < MAX_SIZE; i++) 
+            for (int j = 0; j < MAX_SIZE; j++)
                 if (board[i][j] >= 1)
                     volume++;
         System.out.print(volume);
