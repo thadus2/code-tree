837e8c9770a91fc1106372b7844f8c1844225823
thadus2 <152844617+thadus2@users.noreply.github.com>
2026-04-12 23:09:56 +0900

    Title: 연속되는 수 3, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260412/연속되는 수 3/continuous-number3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        
+        // Please write your code here.
+        int count = 1, max = 1;
+        for (int i = 1; i < n; i++) {
+            if (arr[i] * arr[i - 1] > 0)
+                count++;
+            else   
+                count = 1;
+            if (count > max)
+                max = count;
+        }
+        System.out.println(max);
+    }
+}
