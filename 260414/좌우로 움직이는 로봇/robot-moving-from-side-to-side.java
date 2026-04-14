d06ff5b24643768061f9587289a777609bf9b195
thadus2 <152844617+thadus2@users.noreply.github.com>
2026-04-14 22:44:58 +0900

    Title: 좌우로 움직이는 로봇, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260414/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
+++ 260414/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -3,46 +3,34 @@ import java.util.Scanner;
 public class Main {
     private static final int OFFSET = 1000000;
     private static final int MAX_SIZE = 2 * OFFSET + 10;
-    // private static final int 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        
-        int[] A = new int[MAX_SIZE];
-        int a_pos = OFFSET;
+    private static final int fillPosition(Scanner sc, int n, int[] arr) {
+        int pos = OFFSET;
         for (int i = 0; i < n; i++) {
             int t = sc.nextInt();
             char d = sc.next().charAt(0);
             // Please write your code here.
             if (d == 'L') 
                 while(t-- > 0) {
-                    A[a_pos] = A[a_pos - 1] - 1;
-                    a_pos++;
+                    arr[pos] = arr[pos - 1] - 1;
+                    pos++;
                 }
             if (d == 'R')
                 while(t-- > 0) {
-                    A[a_pos] = A[a_pos - 1] + 1;
-                    a_pos++;
+                    arr[pos] = arr[pos - 1] + 1;
+                    pos++;
                 }
         }
+        return pos;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        
+        int[] A = new int[MAX_SIZE];
+        int a_pos = fillPosition(sc, n, A);
         int[] B = new int[MAX_SIZE];
-        int b_pos = OFFSET;
-        for (int i = 0; i < m; i++) {
-            int t = sc.nextInt();
-            char d = sc.next().charAt(0);
-            // Please write your code here.
-            if (d == 'L') 
-                while(t-- > 0) {
-                    B[b_pos] = B[b_pos - 1] - 1;
-                    b_pos++;
-                }
-            if (d == 'R')
-                while(t-- > 0) {
-                    B[b_pos] = B[b_pos - 1] + 1;
-                    b_pos++;
-                }
-        }
+        int b_pos = fillPosition(sc, m, B);
         
         // Please write your code here.
         while(a_pos < b_pos) {
