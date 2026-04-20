55526a0afa35e4dffc6199ca0b20e2afe5c045d6
thadus2 <152844617+thadus2@users.noreply.github.com>
2026-04-20 23:56:28 +0900

    Title: 작은 구슬의 이동, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260420/작은 구슬의 이동/small-marble-movement.java
+++ 260420/작은 구슬의 이동/small-marble-movement.java
@@ -1,42 +1,37 @@
 import java.util.Scanner;
 
 public class Main {
+    // 격자 범위 내에 있는지 확인하는 함수
     private static boolean inRange(int x, int y, int n) {
         return (x >= 0 && x < n && y >= 0 && y < n);
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int T = sc.nextInt();
-        int R = sc.nextInt();
-        int C = sc.nextInt();
-        char D = sc.next().charAt(0);
-        // Please write your code here.
-        int[][] grid = new int[N][N];
-        grid[R][C] = 1;
-        int[] dx = new int[]{0, 1, 0, -1};
-        int[] dy = new int[]{1, 0, -1, 0};
 
-        int dir = "URDL".indexOf(D);
+        int R = sc.nextInt() - 1; 
+        int C = sc.nextInt() - 1;
+        char D = sc.next().charAt(0);
+
+        int[] dr = {0, -1, 0, 1};
+        int[] dc = {1, 0, -1, 0};
+
+        int dir = "RULD".indexOf(D);
+
         while (T-- > 0) {
-            int nx = R + dx[dir];
-            int ny = C + dy[dir];
-            if (!inRange(nx, ny, N))
+            int nr = R + dr[dir];
+            int nc = C + dc[dir];
+
+            if (!inRange(nr, nc, N)) {
                 dir = (dir + 2) % 4;
-            else {
-                grid[R][C]--;
-                R = nx;
-                C = ny;
-                grid[R][C]++;
-            }
-            System.out.println(R + " " + C);
-        }
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (grid[i][j] == 1) {
-                    System.out.println(i + " " + j);
-                }
+            } else {
+                R = nr;
+                C = nc;
             }
         }
+
+        System.out.println((R + 1) + " " + (C + 1));
     }
 }
