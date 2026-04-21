d500545f9010cfd4480da37e86fee485c827b266
thadus2 <152844617+thadus2@users.noreply.github.com>
2026-04-21 20:14:22 +0900

    Title: 작은 구슬의 이동, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260421/작은 구슬의 이동/small-marble-movement.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    private static boolean inRange(int x, int y, int n) {
+        return (x >= 0 && x < n && y >= 0 && y < n);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int T = sc.nextInt();
+        int R = sc.nextInt() - 1;
+        int C = sc.nextInt() - 1;
+        char D = sc.next().charAt(0);
+        // Please write your code here.
+        int[][] grid = new int[N][N];
+        grid[R][C] = 1;
+        int[] dx = new int[]{0, 1, 0, -1};
+        int[] dy = new int[]{1, 0, -1, 0};
+
+        int dir = "RULD".indexOf(D);
+        while (T-- > 0) {
+            int nr = R + dx[dir];
+            int nc = C + dy[dir];
+            if (!inRange(nr, nc, N)) {
+                dir = (dir + 2) % 4;
+            }
+            else {
+                R = nr;
+                C = nc;
+            }
+        }
+
+        System.out.println((R + 1) + " " + (C + 1));
+
+    }
+}
