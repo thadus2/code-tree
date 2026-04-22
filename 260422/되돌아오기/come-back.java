c1485bdc3db49baf9a4965729e433b77eaca38ec
thadus2 <152844617+thadus2@users.noreply.github.com>
2026-04-22 22:53:25 +0900

    Title: 되돌아오기, Time: 0ms, Memory: 0MB, Status: Compile Error - Codetree

---
+++ 260422/되돌아오기/come-back.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Main {
+    private static int OFFSET = 1000;
+    private static int SIZE = 2 * OFFSET + 10;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] grid = new int[SIZE][SIZE];
+        int[] dx = new int[]{0, 1, 0, -1};
+        int[] dy = new int[]{1, 0, -1, 0};
+
+        int x = OFFSET, y = OFFSET;
+        int time = 0;
+        grid[x][y] = 1;
+        for(int i = 0; i < n; i++){
+            char dir = sc.next().charAt(0);
+            char d = "NESW".indexOf(dir);
+            int dist = sc.nextInt();
+            while(dist-- > 0) {
+                time++;
+                x += dx[d];
+                y += dy[d];
+                if (grid[x][y] == 1) {
+                    System.out.println(time);
+                    return
+                }
+            }
+        }
+    }
+}
