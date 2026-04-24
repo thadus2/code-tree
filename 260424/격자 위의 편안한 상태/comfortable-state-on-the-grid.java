afd23991cd32d56861c381f7b42eaa8569bc62a0
thadus2 <152844617+thadus2@users.noreply.github.com>
2026-04-24 21:56:25 +0900

    Title: 격자 위의 편안한 상태, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260424/격자 위의 편안한 상태/comfortable-state-on-the-grid.java
+++ 260424/격자 위의 편안한 상태/comfortable-state-on-the-grid.java
@@ -13,18 +13,19 @@ public class Main {
         int[] dx = new int[]{0, 1, 0, -1};
         int[] dy = new int[]{1, 0, -1, 0};
 
-        int[][] grid = new int[n + 1][n + 1];
+        int[][] grid = new int[n][n];
         for (int i = 0; i < m; i++) {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
+            int x = sc.nextInt() - 1;
+            int y = sc.nextInt() - 1;
             grid[x][y] = 1;
             int count = 0, dir = 0;
-            for (int j = 0; j < 3; j++) {
+            for (int j = 0; j < 4; j++) {
                 int nx = x + dx[dir];
                 int ny = y + dy[dir];
                 if (inRange(nx, ny, n) && grid[nx][ny] == 1) {
                     count++;
                 }
+                dir++;
             }
             if (count == 3)
                 System.out.println(1);
