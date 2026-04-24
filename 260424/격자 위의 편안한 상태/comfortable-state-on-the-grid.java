a6e6cff671ea8c03a0faf1987c1aa7ccfa153e05
thadus2 <152844617+thadus2@users.noreply.github.com>
2026-04-24 21:40:00 +0900

    Title: 격자 위의 편안한 상태, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260424/격자 위의 편안한 상태/comfortable-state-on-the-grid.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    private static boolean inRange(int x, int y, int n) {
+        return (x >= 0 && x < n && y >= 0 && y < n);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        
+        int[] dx = new int[]{0, 1, 0, -1};
+        int[] dy = new int[]{1, 0, -1, 0};
+
+        int[][] grid = new int[n + 1][n + 1];
+        for (int i = 0; i < m; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            grid[x][y] = 1;
+            int count = 0, dir = 0;
+            for (int j = 0; j < 3; j++) {
+                int nx = x + dx[dir];
+                int ny = y + dy[dir];
+                if (inRange(nx, ny, n) && grid[nx][ny] == 1) {
+                    count++;
+                }
+            }
+            if (count == 3)
+                System.out.println(1);
+            else
+                System.out.println(0);
+        }
+        // Please write your code here.
+    }
+}
