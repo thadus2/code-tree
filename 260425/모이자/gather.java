04eab16d476c3ea8285f88b05f2658c486f6d1f7
thadus2 <152844617+thadus2@users.noreply.github.com>
2026-04-25 23:53:57 +0900

    Title: 모이자, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260425/모이자/gather.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        // Please write your code here.
+        int min_move = 9999;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = 0; j < n; j++) {
+                sum += a[j] * (Math.pow((i - j), 2) / 2);
+            }
+            System.out.println(sum);
+            if (sum < min_move)
+                min_move = sum;
+        }
+        System.out.println(min_move);
+    }
+}
