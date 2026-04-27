a262b651ed1ba1b55fd0ee255a747e4ff4712b40
thadus2 <152844617+thadus2@users.noreply.github.com>
2026-04-27 12:59:49 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260427/빙빙 돌며 사각형 채우기/snail-alphabet-square.java
+++ 260427/빙빙 돌며 사각형 채우기/snail-alphabet-square.java
@@ -12,8 +12,8 @@ public class Main {
         int[] dx = new int[]{0, 1, 0, -1};
         int[] dy = new int[]{1, 0, -1, 0};
         int x = 0, y = 0, dir = 0;
-        for (int i = 65; i < n * m + 65; i++) {
-            grid[x][y] = (char)i;
+        for (int i = 0; i < n * m; i++) {
+            grid[x][y] = (char)((i % 26) + 65);
             int nx = x + dx[dir];
             int ny = y + dy[dir];
             if (!(inRange(nx, ny, n, m) && grid[nx][ny] == 0)) {
