8ead8ff79c51ea9c8ecb96f97586f76b96ca85f4
thadus2 <152844617+thadus2@users.noreply.github.com>
2026-05-01 22:00:38 +0900

    [Passed] 일렬로 서있는 소 2 (135ms, 10MB)

---
+++ 260501/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        // Please write your code here.
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (arr[j] <= arr[k] && arr[i] <= arr[j])
+                        count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
