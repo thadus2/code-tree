fc23eb98c23d69a996aad6a3def998a7f1d36e10
thadus2 <152844617+thadus2@users.noreply.github.com>
2026-05-04 23:47:17 +0900

    [Passed] 인접하지 않은 2개의 수 (144ms, 10MB)

---
+++ 260504/인접하지 않은 2개의 수/two-non-adjacent-numbers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        // Please write your code here.
+        int max = 0;
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 2; j < n; j++) {
+                if (max < (arr[i] + arr[j]))
+                    max = (arr[i] + arr[j]);
+            }
+        }
+        System.out.println(max);
+    }
+}
