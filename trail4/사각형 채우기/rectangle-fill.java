322acacbb481f54aabcfb70e74f608956f821e0f
thadus2 <152844617+thadus2@users.noreply.github.com>
2026-08-30 01:07:04 +0900

    [Passed] 사각형 채우기 (127ms, 10MB)

---
+++ trail4/사각형 채우기/rectangle-fill.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        if (n >= 2) dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+        }
+        System.out.println(dp[n]);
+    }
+}
